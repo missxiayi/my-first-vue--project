3e4dcd8175caeac62b75225f90b8b064d8c92928
missxiayi <15732108381@163.com>
2018-05-08 02:52:14 +0000

    Delete store.js

--- src/store.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY='todos-vuejs'   /*定义常量*/
-export default{  /*导出两个方法*/
-	fetch () {
-		return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
-	},
-	save (items) { /*相当于save:function(items){},es6语言*/
-		window.localStorage.setItem(STORAGE_KEY,JSON.stringify(items))
-	}
-}
